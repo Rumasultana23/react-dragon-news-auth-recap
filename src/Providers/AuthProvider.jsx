b3964c5014a55e7924508739d2f5966673f78447
Rumasultana23 <romaproma23@gmail.com>
2023-10-09 10:47:59 +0600

    updated firebase config

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -8,16 +8,20 @@ const auth = getAuth(app);
 
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     const createUser = (email, password) =>{
+        setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password);
     };
 
     const signIn = (email, password) =>{
+        setLoading(true);
         return signInWithEmailAndPassword(auth,email,password);
     }
 
     const logOut = () =>{
+        setLoading(true);
         return signOut(auth);
 
     }
@@ -25,7 +29,8 @@ const AuthProvider = ({children}) => {
     useEffect(()=>{
         const unSubscribe = onAuthStateChanged(auth, currentUser =>{
             console.log('user in the auth state changed', currentUser);
-            setUser(currentUser)
+            setUser(currentUser);
+            setLoading(false);
         });
         return () =>{
             unSubscribe();
@@ -36,7 +41,8 @@ const AuthProvider = ({children}) => {
         user,
         createUser,
         logOut,
-        signIn
+        signIn,
+        loading
     }
     return (
         <AuthContext.Provider value={authInfo}>
